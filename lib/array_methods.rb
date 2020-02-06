eaafe5bc690b29391d0964b0c41ad67141ecbef5
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-06 04:18:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,6 +16,10 @@ zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
   
+  1.times do |value_to_find|
+    array[value_to_find]
+  end
+  puts array[value_to_find]
 end
 
 def find_max_value(array)
